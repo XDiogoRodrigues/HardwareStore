56376c0cb646e50e2a9a284f95fc5449d648d873
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-06 18:29:48 -0300

    Declarada a classe Produto com seus atributos com seus DataAnnotations

---
+++ HardwareStore/Models/Produto.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HardwareStore.Models;
+
+public class Produto
+{
+    [Key]
+    public int ProdutoId { get; set; }
+
+    [Required]
+    [Display(Name ="Nome da fabricante")]
+    [StringLength(200, MinimumLength = 2, ErrorMessage ="O {0} deve ter no mínimo {1} e no máximo {2}")]
+    public string Fabricante { get; set; }
+
+	[Required]
+	[Display(Name = "Nome do produto")]
+	[StringLength(300, MinimumLength = 2, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
+	public string Nome { get; set; }
+
+	[Required]
+	[Display(Name = "Descrição do produto")]
+	[StringLength(500, MinimumLength = 2, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
+	public string Descricao { get; set; }
+
+	[Required]
+	[Display(Name = "Preço do produto")]
+	[Column(TypeName ="decimal(10,2)")]
+	public decimal Preco { get; set; }
+
+	[Required]
+	[Display(Name = "Quantidade do produto")]
+	public int Quantidade { get; set; }
+
+	[Required]
+	[Display(Name = "Url da imagem")]
+	public string UrlImage { get; set; }
+
+	[Required]
+	[Display(Name = "Url da imagem miniatura")]
+	public string UrlImageMiniatura { get; set; }
+
+    public int CategoriaId { get; set; }
+    public virtual Categoria Categoria { get; set; }
+
+
+}
