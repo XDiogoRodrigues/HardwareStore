cbc4347cf122fe3e0887ce7fe284c7814f81d81b
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-06 19:06:12 -0300

    Realizada outra migração para tabela Categorias com alguns dados

---
+++ HardwareStore/Migrations/20240206220000_PopulandoTabelasCategorias.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HardwareStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulandoTabelasCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome,Descricao) VALUES ('Placas de videos', 'As melhores placa de videos')");
+			migrationBuilder.Sql("INSERT INTO Categorias(Nome,Descricao) VALUES ('Processadores', 'Os melhores processadores')");
+			migrationBuilder.Sql("INSERT INTO Categorias(Nome,Descricao) VALUES ('Placas mães', 'As melhores placa mães')");
+			migrationBuilder.Sql("INSERT INTO Categorias(Nome,Descricao) VALUES ('Gabinetes', 'Os melhores Gabinetes')");
+
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
