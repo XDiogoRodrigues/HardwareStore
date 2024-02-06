2768f9c67ea6070d019d4b0f5fd8a2759183831a
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-06 17:42:09 -0300

    Declarada a classe Categoria com seus atributos com seu DataAnnotations

---
+++ HardwareStore/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HardwareStore.Models;
+
+public class Categoria
+{
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required]
+    [Display(Name = "Nome da categoria")]
+    [StringLength(300, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
+    public string Nome{ get; set; }
+
+	[Required]
+	[Display(Name = "Descrição da categoria")]
+	[StringLength(300, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo {2}")]
+    public string Descricao { get; set; }
+
+}
