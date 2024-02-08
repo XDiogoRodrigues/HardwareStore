6e1221451f285e5c90722507545736bb76b3b71f
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-08 19:42:04 -0300

    Criado as views: Create,Delete,Details,Edit,Index no Controller Categoria. aplicado alguns estilo CSS

---
+++ HardwareStore/Migrations/20240208162332_NovasCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HardwareStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class NovasCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome,Descricao) VALUES('Computadores', 'Os melhores computadores estão aqui')");
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome,Descricao) VALUES('Celulares', 'Os melhores celulares estão aqui')");
+            migrationBuilder.Sql("INSERT INTO Categorias(Nome,Descricao) VALUES('Coolers', 'Os melhores coolers para deixar seu computador refrigerado estão aqui')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
