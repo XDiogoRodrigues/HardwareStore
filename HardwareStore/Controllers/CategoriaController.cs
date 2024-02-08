7a8285a8a5c98ef71621ba278d222ca484637891
Diogo Rodrigues <diogosouzak15@gmail.com>
2024-02-08 13:06:20 -0300

    Declarado o Controller responsavel pelo Model Categoria. Criado a View que possibilita a visualização de todas Categorias no Banco de dados

---
+++ HardwareStore/Controllers/CategoriaController.cs
@@ -0,0 +1,21 @@
+﻿using HardwareStore.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HardwareStore.Controllers
+{
+	public class CategoriaController : Controller
+	{
+		private readonly IRepositorioCategoria _repositorioCategoria;
+
+		public CategoriaController(IRepositorioCategoria repositorioCategoria)
+		{
+			_repositorioCategoria = repositorioCategoria;
+		}
+
+		public IActionResult ListaDeCategoria()
+		{
+			var categorias = _repositorioCategoria.Categorias;
+			return View(categorias);
+		}
+	}
+}
